791416ad29b6c9b039f7640188e006dabe589c5f
Cristian Florez <ingenieroflorezco@gmail.com>
2026-06-07 19:12:18 -0500

    fix: self-contained sketch for Wokwi compatibility

--- firmware/esp32/sketch.ino
+++ firmware/esp32/sketch.ino
@@ -1,44 +1,67 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include "src/config.h"
-#include "src/NodusState.h"
-#include "src/NodusFace.h"
-#include "src/NodusDisplay.h"
-#include "src/NodusAnimator.h"
 
-NodusState state;
-NodusFace face;
-NodusDisplay display;
-NodusAnimator animator;
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+#define STATE_INTERVAL 3000
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+enum Mood { HAPPY, SLEEPY, THINKING, SAD, ALERT, MOOD_COUNT };
+
+const char* moodNames[] = { "HAPPY", "SLEEPY", "THINKING", "SAD", "ALERT" };
+const char* moodFaces[] = { "(^^)", "(--)", "(..?)", "(T_T)", "(><)" };
+
+Mood currentMood = HAPPY;
 unsigned long lastChange = 0;
-bool needsRedraw = true;
+
+void drawFace() {
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+
+  // Face centered
+  display.setTextSize(2);
+  const char* face = moodFaces[currentMood];
+  int16_t x = (SCREEN_WIDTH - strlen(face) * 12) / 2;
+  display.setCursor(x, 20);
+  display.print(face);
+
+  // Mood name at bottom
+  display.setTextSize(1);
+  const char* name = moodNames[currentMood];
+  int16_t nx = (SCREEN_WIDTH - strlen(name) * 6) / 2;
+  display.setCursor(nx, 54);
+  display.print(name);
+
+  display.display();
+}
 
 void setup() {
-  Serial.begin(SERIAL_BAUD_RATE);
+  Serial.begin(115200);
   Serial.println("\nNodus starting...\n");
-  display.begin();
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println("SSD1306 failed!");
+    while (true);
+  }
+
+  drawFace();
 }
 
 void loop() {
   unsigned long now = millis();
-
-  if (now - lastChange >= STATE_CHANGE_INTERVAL_MS) {
+  if (now - lastChange >= STATE_INTERVAL) {
     lastChange = now;
-    state.next();
-    needsRedraw = true;
+    currentMood = (Mood)((currentMood + 1) % MOOD_COUNT);
+
     Serial.print("Mood: ");
-    Serial.println(state.name());
-    Serial.println(face.render(state.current()));
+    Serial.println(moodNames[currentMood]);
+    Serial.println(moodFaces[currentMood]);
     Serial.println();
-  }
 
-  bool wasBlink = animator.isBlinking();
-  animator.update();
-  if (animator.isBlinking() != wasBlink) needsRedraw = true;
-
-  if (needsRedraw) {
-    display.showFace(state.current(), animator.isBlinking());
-    needsRedraw = false;
+    drawFace();
   }
 }
